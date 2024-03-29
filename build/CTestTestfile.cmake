a372b86a1abfe92bb48bee76f47f0f2de9278e37
Stanislas Laurent <lauresta@students.zhaw.ch>
2024-03-29 12:37:28 +0100

    Removed build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/stnsl/OneDrive/Documents/Studies/ZHAW/S4/SNP/Praktika/P06_Personen_Verwaltung_Linked_List
-# Build directory: C:/Users/stnsl/OneDrive/Documents/Studies/ZHAW/S4/SNP/Praktika/P06_Personen_Verwaltung_Linked_List/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[P06_Personen_Verwaltung_Linked_List_tests]=] "C:/Users/stnsl/OneDrive/Documents/Studies/ZHAW/S4/SNP/Praktika/P06_Personen_Verwaltung_Linked_List/build/tests.exe")
-set_tests_properties([=[P06_Personen_Verwaltung_Linked_List_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/stnsl/OneDrive/Documents/Studies/ZHAW/S4/SNP/Praktika/P06_Personen_Verwaltung_Linked_List/CMakeLists.txt;18;add_test;C:/Users/stnsl/OneDrive/Documents/Studies/ZHAW/S4/SNP/Praktika/P06_Personen_Verwaltung_Linked_List/CMakeLists.txt;0;")
